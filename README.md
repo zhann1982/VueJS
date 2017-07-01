0e28c303edb652375933e4235621d5e62214050a
Zhanibek Seidzhan <zhann2013@gmail.com>
2017-07-01 19:01:43 +0600

    First upload of main files from my project folder

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BootstrapTutorials
+
+## This project is based on different tutorials from the web.
+
 Basics of bootstrap
---
+++ main.js
@@ -0,0 +1,9 @@
+var $ = require("jquery");
+var tether = require("tether");
+var bootstrap = require("bootstrap");
+
+
+
+$(document).ready(function(){
+	$('img').css('border-radius','19px');
+});
