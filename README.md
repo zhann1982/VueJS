0ae1cab9bb68167f6e0f93a16fe7db243b6f6f6f
Zhanibek Seidzhan <zhann2013@gmail.com>
2021-03-03 22:37:37 +0600

    Deleted everything
    Deleted everything, because repository was renamed to VueJS

--- README.md
+++
@@ -1,4 +0,0 @@
-# Vue JS
-## This project is based on different tutorials from the web.
-
-Basics of Vue.js
--- main.js
+++
@@ -1,9 +0,0 @@
-var $ = require("jquery");
-var tether = require("tether");
-var bootstrap = require("bootstrap");
-
-
-
-$(document).ready(function(){
-	$('img').css('border-radius','19px');
-});
